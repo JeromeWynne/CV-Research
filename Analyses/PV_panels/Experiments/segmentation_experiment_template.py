5b72199c4d84a6da689ac1252fe5204200e486fa
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-08-31 17:12:50 +0100

    Radically restructured graph - queued input, adadelta optimizer, session monitoring.

--- Analyses/PV_panels/Experiments/segmentation_experiment_template.py
+++ Analyses/PV_panels/Experiments/segmentation_experiment_template.py
@@ -1,38 +1,39 @@
-from cnn_utils import augment_image
+from cnn_utils import augment_image, balance_dataset
 from scipy.misc import imread
 from shutil import rmtree
 import tensorflow as tf
 from glob import glob
 from os import mkdir
+import pandas as pd
 import numpy as np
 
 """
     Before running this script, make sure that:
-        1. A ./Results directory has been created.
-        2. A ./SavedModels directory has been created.
+        1. A ./results directory has been created.
+        2. A ./saved_models directory has been created.
         3. The DATA_DIR has been set correctly.
         4. The MODEL_ID has been updated to reflect the chosen segmentation model.
 """
 
 """ Configuration """
-TRAINING_EPOCHS = 10
+TRAINING_EPOCHS = 30
 MINIBATCH_SIZE  = 32
-INITIAL_LEARNING_RATE = 0.01
-MODEL_ID        = "001_SegmentationExample"
+INITIAL_LEARNING_RATE = 0.1
+DECAY_STEPS     = 20
+DECAY_RATE      = 0.95
+MODEL_ID        = "002_DeepMask"
 DATA_DIR        = './data/pv_panels/'
-import segmentation_model_001_example as segmentation_model
+import segmentation_model_002_CrackMask as segmentation_model
 
 np.random.seed(42)
 
 
-""" Numpy data import """
+""" Import data filepaths """
 print('Reading data...')
-data = { 'training_images'  : np.array([imread(fp, mode = 'L') for fp in glob(DATA_DIR + 'training/images/*.png')]),
-         'training_masks'   : np.array([imread(fp, mode = 'L') for fp in glob(DATA_DIR + 'training/masks/*.png')]),
-         'validation_images': np.array([imread(fp, mode = 'L') for fp in glob(DATA_DIR + 'validation/images/*.png')]),
-         'validation_masks' : np.array([imread(fp, mode = 'L') for fp in glob(DATA_DIR + 'validation/masks/*.png')])
-       } # These are all in [0, 255]
-
+names = { 
+    'training'   : [fp for fp in pd.read_csv(DATA_DIR + '/training/training_scheme.csv')['name']],
+    'validation' : [fp for fp in pd.read_csv(DATA_DIR + '/validation/validation_scheme.csv')['name']]
+}
 
 """ Computational graph 
 
@@ -52,32 +53,48 @@ graph = tf.Graph()
 with graph.as_default():
     
     tf.set_random_seed(42)
-    
-    """ Graph - TensorFlow data import """
-    with tf.name_scope('Import'):
-        training_dataset = tf.contrib.data.Dataset.from_tensor_slices(
-                                    ( data['training_images'][:, :, :, np.newaxis].astype(np.float32),
-                                      data['training_masks'][:, :, :, np.newaxis].astype(np.float32)/255. )
-                                    )
-
-        validation_dataset = tf.contrib.data.Dataset.from_tensor_slices(
-                                    ( data['validation_images'][:, :, :, np.newaxis].astype(np.float32),
-                                      data['validation_masks'][:, :, :, np.newaxis].astype(np.float32)/255. )
-                                    )
         
-    """ Graph - Preprocessing """
-    with tf.name_scope('Preprocessing'):
-
-        training_dataset   = training_dataset.repeat(TRAINING_EPOCHS) # the training dataset is not balanced, nor is the validation set.
-        training_dataset   = training_dataset.map(lambda q, gt: augment_image(q, gt, mode = 'training'))
-        training_dataset   = training_dataset.batch(MINIBATCH_SIZE)
+    def get_batch(names, mode ,epochs):
+        """
+            Accepts a dictionary; names = {'training':..., 'validation':...} that contains lists of image names.
+                
+            Returns an op that fetches a batch of images and their one-hot encoded masks.
+            
+            (images, masks) -> queue #1 -> augmentation -> queue #2 -> minibatches of (images, masks)
         
-        validation_dataset = validation_dataset.map(lambda q, gt: augment_image(q, gt, mode = 'validation'))
-        validation_dataset = validation_dataset.batch(data['validation_images'].shape[0])
-    
-    with tf.name_scope('IteratorInitialization'): # The iterators must be manually initialized before the dataset can be iterated over
-        training_iterator   = tf.contrib.data.Iterator.from_dataset(training_dataset)
-        validation_iterator = tf.contrib.data.Iterator.from_dataset(validation_dataset)
+            1. image_queue and mask_queue contain queues of image and mask filepaths, respectively.
+            2. The WholeFileReader().read() method reads these filepaths and returns a string containing the file contents.
+            3. The decode_png() method converts this string into uint image tensor.
+            4. augment_image() standardizes the images ( + flips, rotates, and contrast-shifts them if mode == 'training').
+               The spatial augmentations are also applied to the masks. The masks are one-hot encoded, meaning that they
+               are returned as two-channel images. All tensors returned have dtype float32.
+            5. shuffle_batch() forms another queue that allows us to randomly batch our images from a pool of size min_after_dequeue.
+        
+        """
+
+        image_queue = tf.train.string_input_producer(
+                                            [DATA_DIR + '/' + mode + '/images/' + n for n in names[mode]], 
+                                                shuffle = False, num_epochs = epochs, name = mode + '_image_queue')
+        mask_queue  = tf.train.string_input_producer(
+                                            [DATA_DIR + '/' + mode + '/masks/' + n for n in names[mode]], 
+                                                shuffle = False, num_epochs = epochs)
+            
+        reader = tf.WholeFileReader()
+            
+        _, image_string = reader.read(image_queue)
+        _, mask_string  = reader.read(mask_queue)
+           
+        image = tf.image.decode_png(contents = image_string, channels = 1)
+        mask  = tf.image.decode_png(contents = mask_string, channels = 1)
+            
+        image, mask = augment_image(image, mask, mode = mode)
+        image_batch, mask_batch = tf.train.shuffle_batch([image, mask],
+                                                          batch_size = MINIBATCH_SIZE,
+                                                          capacity   = MINIBATCH_SIZE*13,
+                                                          min_after_dequeue = MINIBATCH_SIZE*10,
+                                                          shapes = [[185, 185, 1], [185, 185, 2]])
+        return image_batch, mask_batch
+        
     
     """ Graph - Model Variables """
     with tf.name_scope('Variables'):
@@ -86,27 +103,33 @@ with graph.as_default():
     
     """ Graph - Predictions and Optimization """
     with tf.name_scope('Training'):
-        training_images, training_masks = training_iterator.get_next()
+        training_images, training_masks = get_batch(names, mode = 'training',  epochs = TRAINING_EPOCHS)
         training_loss, training_predictions, training_iou = segmentation_model.graph(variables, training_images,
                                                                                      training_masks, mode = 'training')
         
-        training_summary = tf.summary.merge([
-                                tf.summary.scalar('MeanCrossEntropy', training_loss),
-                                tf.summary.scalar('MeanIoU',          training_iou),
-                                tf.summary.image( 'Predictions',      training_predictions, max_outputs = 1),
-                                tf.summary.image( 'Queries',          training_images,      max_outputs = 1),
-                                tf.summary.image( 'GroundTruth',      
-                                                  tf.slice(training_masks, begin = [0, 0, 0, 0],size = [-1, -1, -1, 1]),
-                                                  max_outputs = 1)
-                             ])
-        
         with tf.name_scope('Optimization'):
-            optimizer = tf.train.GradientDescentOptimizer(learning_rate = INITIAL_LEARNING_RATE)
-            gradients = optimizer.compute_gradients(training_loss)
-            optimize  = optimizer.apply_gradients(gradients, global_step = training_step)
+            #learning_rate = tf.train.exponential_decay(INITIAL_LEARNING_RATE, global_step = training_step,
+            #                                           decay_steps = DECAY_STEPS, decay_rate = DECAY_RATE)
+            #optimize = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss = training_loss,
+            #                                                                     global_step = training_step)
+            optimize = tf.train.AdadeltaOptimizer(1.).minimize(loss = training_loss,
+                                                               global_step = training_step)
+        
+        training_summary = tf.summary.merge([
+            tf.summary.scalar('MeanCrossEntropy', training_loss),
+            tf.summary.scalar('MeanIoU',          training_iou),
+            tf.summary.image( 'Predictions',      training_predictions, max_outputs = 1),
+            tf.summary.image( 'Queries',          training_images,      max_outputs = 1),
+            tf.summary.image( 'GroundTruth',      
+                             tf.slice(training_masks, begin = [0, 0, 0, 0], size = [-1, -1, -1, 1]),
+                             max_outputs = 1),
+            tf.get_default_graph().get_tensor_by_name("Training/training_image_queue/fraction_of_32_full:0"),
+            tf.get_default_graph().get_tensor_by_name("Training/input_producer/fraction_of_32_full:0")
+            #tf.summary.scalar('LearningRate', learning_rate)
+        ])
         
     with tf.name_scope('Validation'):
-        validation_images, validation_masks = validation_iterator.get_next()
+        validation_images, validation_masks = get_batch(names, mode = 'validation', epochs = 1)
         validation_loss, validation_predictions, validation_iou = segmentation_model.graph(variables, validation_images,
                                                                                            validation_masks, mode = 'validation')
         validation_summary = tf.summary.merge([
@@ -135,6 +158,9 @@ with graph.as_default():
         6. Save the model parameters, close the writers.
 """
 print('Graph defined.\nInitializing session...')
+
+validation_step = 0
+
 with tf.Session(graph = graph) as session:
     try:
         mkdir('./results/' + MODEL_ID)
@@ -150,21 +176,42 @@ with tf.Session(graph = graph) as session:
     training_writer   = tf.summary.FileWriter('./results/' + MODEL_ID + '/training', session.graph)
     validation_writer = tf.summary.FileWriter('./results/' + MODEL_ID + '/validation', session.graph)
     
-    session.run([tf.global_variables_initializer(),
-                 training_iterator.initializer, validation_iterator.initializer])
-    print('Session initialized. \nTraining for {} epochs.'.format(TRAINING_EPOCHS))
+    session.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
     
+    coordinator = tf.train.Coordinator()
+    threads     = tf.train.start_queue_runners(sess = session, coord = coordinator)
+                                               
+    print('Session initialized. \nTraining for {} epochs.'.format(TRAINING_EPOCHS))
+    step_number = 0
     while True:
         try:
-            _, step_summary = session.run([optimize, training_summary])
-            step_number     = tf.train.global_step(session, training_step)
-            training_writer.add_summary(step_summary, step_number)
+            if step_number % 100 == 1: # Record session performance every 100 steps
+                run_metadata = tf.RunMetadata()
+                _, step_summary = session.run([optimize, training_summary],
+                                               options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
+                                               run_metadata = run_metadata)
+                step_number     = tf.train.global_step(session, training_step)
+                training_writer.add_run_metadata(run_metadata, 'Step_{}'.format(step_number))
+                training_writer.add_summary(step_summary, step_number)
+            else:
+                _, step_summary = session.run([optimize, training_summary])
+                step_number     = tf.train.global_step(session, training_step)
+                training_writer.add_summary(step_summary, step_number)
             
         except tf.errors.OutOfRangeError: # When we reach the end of the training dataset, test the model
-            validation_summary = session.run(validation_summary)
-            validation_writer.add_summary(validation_summary, step_number)
+            print('Training complete.\nValidating model...')
+            try:
+                validation_summary = session.run(validation_summary)
+                validation_step += 1
+                validation_writer.add_summary(validation_summary, validation_step)
+                    
+            except tf.errors.OutOfRangeError:
+                print('Model validated. Experiment complete.')
+                coordinator.request_stop() # Close the threads
+                break
             break
             
+    coordinator.join(threads) # What does this do?
     training_writer.close()
     validation_writer.close()
     saver.save(session, './saved_models/' + MODEL_ID)
